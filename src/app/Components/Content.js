a19c81c09c7105fec72f3061ad5a76af04df1241
SushmaBalami <balamianahita@gmail.com>
2024-02-22 11:42:13 +0545

    Edited Dashboard

--- src/app/Components/Content.js
+++ src/app/Components/Content.js
@@ -1,8 +1,11 @@
 "use client";
 import Link from "next/link";
+import { BsThreeDots } from "react-icons/bs";
+import { PiNumberZeroBold } from "react-icons/pi";
 
 const Content = () => {
   return (
+    <>
     <div
       className="border-2 border-gray-100 shadow-md"
       style={{
@@ -20,37 +23,94 @@ const Content = () => {
         borderRadius: "8px",
       }}
     >
-    <div className="p mx-4 mt-10 text-xl font-semibold">
+      <div className="p mx-4 mt-10 text-xl font-semibold">
         Demat Application Overview:
+      </div>
+      <div
+        className="content"
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          marginTop: "20px",
+        }}
+      >
+        <div
+          className="items border border-gray-100 shadow"
+          style={{
+            width: "250px",
+            height: "100px",
+            borderRadius: "5px",
+            marginRight: "10px",
+            marginLeft: "10px",
+            display: "flex",
+            justifyContent: "space-around",
+            fontSize: "14px",
+          }}
+        >
+          <Link href="#" className="mt-1">
+            Received Application
+          </Link>
+          <BsThreeDots className="text-gray-400 mt-1" />
+          <PiNumberZeroBold className="absolute text-red-500 font-extrabold mt-16 w-6 h-5"/>
+        </div>
+        <div
+          className="items border border-gray-100 shadow"
+          style={{
+            width: "250px",
+            height: "100px",
+            borderRadius: "5px",
+            marginRight: "10px",
+            marginLeft: "10px",
+            display: "flex",
+            justifyContent: "space-around",
+            fontSize: "14px",
+          
+          }}
+        >
+          <Link href="#" className="mt-1">
+            Verified Application
+          </Link>
+          <BsThreeDots className="text-gray-400 mt-1" />
+          <PiNumberZeroBold className="absolute text-blue-500 font-extrabold mt-16 w-6 h-5"/>
+        </div>
+        <div
+          className="items border border-gray-100 shadow"
+          style={{
+            width: "250px",
+            height: "100px",
+            borderRadius: "5px",
+            marginRight: "10px",
+            marginLeft: "10px",
+            display: "flex",
+            justifyContent: "space-around",
+            fontSize: "14px",
+          }}
+        >
+          <Link href="#"  className="mt-1">Approved Application</Link>
+          <BsThreeDots className="text-gray-400 mt-1" />
+          <PiNumberZeroBold className="absolute text-brown-500 font-extrabold mt-16 w-6 h-5"/>
+        </div>
+        <div
+          className="items border border-gray-100 shadow"
+          style={{
+            width: "250px",
+            height: "100px",
+            borderRadius: "5px",
+            marginRight: "10px",
+            marginLeft: "10px",
+            display: "flex",
+            justifyContent: "space-around",
+            fontSize: "14px",
+          }}
+        >
+          <Link href="#"  className="mt-1">Rejected Application</Link>
+          <BsThreeDots className="text-gray-400 mt-1"/>
+          <PiNumberZeroBold className="absolute text-yellow-500 font-extrabold mt-16 w-6 h-5"/>
+        </div>
+      </div>
     </div>
-    <div className="content" style={{
-         display: "grid",
-         gridTemplateColumns: "auto",
-         padding: "10px",
-    }}>
-       <div className="items">
-        <Link href="#">
-        Received Application
-        </Link>
-       </div>
-       <div className="items">
-        <Link href="#">
-        Verified Application
-        </Link>
-       </div>
-       <div className="items">
-        <Link href="#">
-        Approved Application
-        </Link>
-       </div>
-       <div className="items">
-        <Link href="#">
-        Rejected Application
-        </Link>
-       </div>
-
-    </div>
-    </div>
+    
+    </>
   );
 };
 export default Content;
